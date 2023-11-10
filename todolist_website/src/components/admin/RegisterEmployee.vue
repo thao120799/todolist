dc3cda5b44532dfe986ab55397239ac422ea61a3
thanhthao <thanhthao120799@gmail.com>
2023-11-10 17:13:46 +0900

    add employee

--- todolist_website/src/components/admin/RegisterEmployee.vue
+++ todolist_website/src/components/admin/RegisterEmployee.vue
@@ -1,18 +1,58 @@
 <template>
-    <form class="p-3 mt-3">
+    <form @submit.prevent="addEmployee" class="p-3 mt-3">
         <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <label for="firstName" class="form-label">First Name</label>
+            <input v-model="firstName" class="form-control" id="firstName" required>
         </div>
         <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
+            <label for="lastName" class="form-label">Last Name</label>
+            <input v-model="lastName" class="form-control" id="lastName" required>
         </div>
-        <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        <div class="mb-3">
+            <label for="emai1" class="form-label">Email address</label>
+            <input v-model="email" class="form-control" id="email" required>
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input v-model="password" class="form-control" id="password" required>
+        </div>
+        <div>
+            <h3>Data retrieved from server:</h3>
+            <p v-if="success"> {{ success }}</p>
+            <pre>{{ response }}</pre>
         </div>
         <button type="submit" class="btn btn-primary">Register</button>
         <p @click="$router.push('/')">Login</p>
     </form>
 </template>
+
+<script>
+import EmployeeService from '/home/thao/toDoList/todolist_website/src/services/EmployeeService.js';
+
+export default {
+    data() {
+        return {
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+            success: "",
+            response: "",
+        }
+    },
+    methods: {
+        addEmployee() {
+            EmployeeService.addEmployees(this.firstName, this.lastName, this.email,this.password).then(response => {
+                this.success = 'Data saved successfully';
+                this.response = JSON.stringify(response, null, 2)
+            }).catch(error => {
+                this.response = 'Error: ' + error.response.status
+            })
+            this.firstName = '';
+            this.lastName = '';
+            this.email = '';
+            this.password = '';
+        }
+    }
+}
+</script>
--- todolist_website/src/services/EmployeeService.js
+++ todolist_website/src/services/EmployeeService.js
@@ -7,6 +7,20 @@ class EmployeeService {
     getEmployees() {
         return axios.get(EMPLOYEE_API_BASE_URL);
     }
+
+    addEmployees(firstName, lastName, gmail, password) {
+        return axios.post(EMPLOYEE_API_BASE_URL, {
+            employeeFirstName: firstName,
+            employeeLastName: lastName,
+            employeeGmail: gmail,
+            password: password,
+        });
+    }
+
+    editEmployees() {
+        
+    }
+
 }
 
 export default new EmployeeService();
